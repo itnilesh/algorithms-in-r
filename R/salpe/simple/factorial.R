8fd3f50a799c05d59265f8f89363b7f6dcae862f
Nilesh Salpe <itnilesh@gmail.com>
2018-04-06 23:52:26 +0800

    First R code

---
+++ R/salpe/simple/factorial.R
@@ -0,0 +1,11 @@
+# Take input from  user
+
+num=as.integer(readline(prompt = "Please enter number = "))
+facto5 = 1;
+for(i in 1: num) {
+  
+  facto = facto*i
+}
+
+print(paste("Factorial of number is = " , facto))
+s
